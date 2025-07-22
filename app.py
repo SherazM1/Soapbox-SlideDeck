c0db67c50bb25bd91266db2c51762af8c793692d
Sheraz Mukhtar <Smukhtar@uark.edu>
2025-07-22 09:07:52 -0500

    Describe your changes here

--- app.py
+++ app.py
@@ -193,7 +193,7 @@ def populate_pptx_from_excel(excel_df, pptx_template_path, output_path):
                         if "#" in run.text:
                             run.text = run.text.replace("#", str(engagement_rate_value))
                 # Engagements (main)
-                elif "Engagements" in text and "% increase" not in text:
+                elif "Engagements" in text:
                     for run in para.runs:
                         if "#" in run.text:
                             run.text = run.text.replace("#", str(engagements_value))
