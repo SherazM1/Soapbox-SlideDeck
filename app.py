38fc3ea0e3039a8763dece37eb7abffea99c975d
Sheraz Mukhtar <smukhtar@uark.edu>
2025-07-28 13:27:53 -0500

    Describe your changes here

--- app.py
+++ app.py
@@ -663,14 +663,14 @@ def populate_pptx_from_excel(excel_df, pptx_template_path, output_path, images=N
                 for para in shape.text_frame.paragraphs:
                     text = para.text.strip()
                     for run in para.runs:
-                        if "@influencerhandle" in run.text:
-                            run.text = run.text.replace("@influencerhandle", handle_slide_7_left)
+                        if "influencerhandle" in run.text:
+                            run.text = run.text.replace("influencerhandle", handle_slide_7_left)
             elif "Paid" in shape.text:
                 for para in shape.text_frame.paragraphs:
                     text = para.text.strip()
                     for run in para.runs:
-                        if "@influencerhandle" in run.text:
-                            run.text = run.text.replace("@influencerhandle", handle_slide_7_right)
+                        if "influencerhandle" in run.text:
+                            run.text = run.text.replace("influencerhandle", handle_slide_7_right)
 
     
     #slide 12
