0f94e6e4697ac2c7564682bf72c6c584bbd6d81f
Sheraz Mukhtar <smukhtar@uark.edu>
2025-07-28 09:47:09 -0500

    Describe your changes here

--- app.py
+++ app.py
@@ -579,6 +579,7 @@ def populate_pptx_from_excel(excel_df, pptx_template_path, output_path, images=N
         if shape.has_text_frame and shape.name == "TextBox 6":
             for para in shape.text_frame.paragraphs:
                 for run in para.runs:
+                    print(f"Run: '{run.text}'")
                     if "# Engagements" in run.text:
                         run.text = run.text.replace("#", str(paid_engagements))
                         run.text = run.text.replace("Engagements", "Engagements")
