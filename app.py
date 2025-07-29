6ee55acb6ff2799f8a9a63fb1ded5d13721bc759
Sheraz Mukhtar <smukhtar@uark.edu>
2025-07-29 12:09:24 -0500

    Describe your changes here

--- app.py
+++ app.py
@@ -714,9 +714,12 @@ def populate_pptx_from_excel(excel_df, pptx_template_path, output_path, images=N
                         para.clear()
                         para.add_run().text = bullet_texts[i]
         # Is this the ThruPlays box? (e.g., contains "# ThruPlays")
+    
+    slide = prs.slides[11]
     for shape in slide.shapes:
         if shape.has_text_frame and shape.name == "TextBox 6":
             for para in shape.text_frame.paragraphs:
+                text = para.text.strip()
             # Only touch paragraphs with '# ThruPlays' in them
                 if "# ThruPlays" in para.text:
                     para.clear()
